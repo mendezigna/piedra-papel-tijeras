fd20a9bdb423d20e23fc47e869f93750c39ef87f
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-02-27 18:55:47 -0300

    Updates

---
+++ precache-manifest.4bf15231cb88b6e06fca888f863962d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1dce7d7701da8349b48f714843967d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f47de6aae713945172",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "902d323c4af1f50fdbdc",
+    "url": "/static/css/main.eee687a8.chunk.css"
+  },
+  {
+    "revision": "34f47de6aae713945172",
+    "url": "/static/js/2.64934a18.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.64934a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902d323c4af1f50fdbdc",
+    "url": "/static/js/main.5dbc836d.chunk.js"
+  },
+  {
+    "revision": "23ad1b59f14e4f878ef0",
+    "url": "/static/js/runtime-main.e424608b.js"
+  },
+  {
+    "revision": "676ea25987c5fb398fc53eaadf506bf3",
+    "url": "/static/media/img-modal.676ea259.webp"
+  },
+  {
+    "revision": "cdaf8eca185df8dce4a5b11b90614b29",
+    "url": "/static/media/lagarto.cdaf8eca.png"
+  },
+  {
+    "revision": "b961c00da7d85bbc6572635c945b7504",
+    "url": "/static/media/papel.b961c00d.png"
+  },
+  {
+    "revision": "b9fcd154848d41b43be1a61d346dc0ab",
+    "url": "/static/media/piedra.b9fcd154.png"
+  },
+  {
+    "revision": "17a3fa381faaba5acbfe33046ced825e",
+    "url": "/static/media/reglas.17a3fa38.jpg"
+  },
+  {
+    "revision": "a4abb9928484a979327828f29f862b07",
+    "url": "/static/media/spock.a4abb992.png"
+  },
+  {
+    "revision": "3783bd3a363de288961fb65ac5730d22",
+    "url": "/static/media/tijera.3783bd3a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pie-pap-tij-lag-spo/precache-manifest.955bea8c6becd5b9926a5552b7008846.js"
+  "/precache-manifest.4bf15231cb88b6e06fca888f863962d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pie-pap-tij-lag-spo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
